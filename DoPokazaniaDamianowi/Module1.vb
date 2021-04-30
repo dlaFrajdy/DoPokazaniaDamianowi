91817054ad910959e9f48d981a97cbdda2cb0e72
Bartek Siedlecki <barteksiedlecki@gmail.com>
2021-04-30 23:27:23 +0200

    Dodano funkcje X

--- DoPokazaniaDamianowi/Module1.vb
+++ DoPokazaniaDamianowi/Module1.vb
@@ -2,6 +2,32 @@
 
     Sub Main()
 
+        Console.WriteLine("Cześć")
+        Console.ReadKey()
+
+
+        'funkcja X
+        'fsdf
+        'SortedDictionary(Of
+        '    FieldAccessException
+        '    SortedDictionary(Of
+        '    FieldAccessException
+        '    SortedDictionary(Of
+        '    FieldAccessException
+        '    SortedDictionary(Of
+        '    DateFormat
+
+        'SortedDictionary(Of
+        '    FieldAccessException
+
+        'SortedDictionary(Of
+        '    FieldAccessException
+        '    SortedDictionary(Of
+        '    DateFormat
+        '    FieldAccessException
+        'SortedDictionary(Of
+        '    DateFormat
+
     End Sub
 
 End Module
